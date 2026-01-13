d86c3b12fb3d757e321fa1038f6b988cd355fb31
kaushar-mecaps <akaushar690@gmail.com>
2026-01-13 21:03:28 +0530

    Add New Project Shopping Cart App

---
+++ Variable Based Mini Projects (Code )/5.Shopping Cart Billing/src/ShoppingCartApp.java
@@ -0,0 +1,12 @@
+public class ShoppingCartApp {
+    public static void main(String[] args) {
+
+        var price = 10.20;
+        var quantity = 5;
+        var billAmount = price * quantity;
+
+        System.out.println("Price: " + price);
+        System.out.println("Quantity: " + quantity);
+        System.out.println("Bill Amount: " + billAmount);
+    }
+}
