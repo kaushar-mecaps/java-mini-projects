6743cb2d8a3471e6cf871f8a5e500ce50f95aa55
kaushar-mecaps <akaushar690@gmail.com>
2026-03-07 14:47:47 +0530

    Add New Java Topic Based Mini Project (Students Fees And Result Managements System

---
+++ 2.Data Type Based Mini Projects/1.Students Fees And Result Management System/src/StudentFeeSystem.java
@@ -0,0 +1,97 @@
+import java.util.Scanner;
+
+public class StudentFeeSystem {
+    public static void main(String[] args) {
+
+        String name ;
+        int rollNo ;
+        byte age ;
+        char gender;
+
+        int hindi ;
+        int english ;
+        int mathematic ;
+        int science ;
+        int socalScience ;
+        int drawing ;
+
+        int totalMarks ;
+        float percentage ;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("-------Student Personal Information:--------");
+        System.out.print("Enter Name        : ");
+        name = input.nextLine();
+        System.out.print("Enter Roll Number : ");
+        rollNo = input.nextInt();
+        System.out.print("Enter Age         : ");
+        age = input.nextByte();
+        System.out.print("Enter Gender (M/F): ");
+        gender = input.next().charAt(0);
+
+        System.out.println("-------Student Subject Details:---------");
+        System.out.print("Enter Marks in Hindi: ");
+        hindi = input.nextInt();
+        System.out.print("Enter Marks in english: ");
+        english = input.nextInt();
+        System.out.print("Enter Marks in mathematic: ");
+        mathematic = input.nextInt();
+        System.out.print("Enter Marks in science: ");
+        science = input.nextInt();
+        System.out.print("Enter Marks in socalScience: ");
+        socalScience = input.nextInt();
+        System.out.print("Enter Marks in drawing: ");
+        drawing = input.nextInt();
+
+        totalMarks =
+                hindi + english + mathematic + science + socalScience + drawing;
+        percentage = (totalMarks / 600.0f) * 100;
+
+        System.out.println("--------Student Marks details:--------");
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Percentage: " + percentage + "%");
+
+        System.out.println("--------Student Grade details:----------");
+        if (percentage >= 75) {
+            System.out.println("Grade: A");
+        } else if (percentage >= 60) {
+            System.out.println("Grade: B");
+        } else if (percentage >= 45) {
+            System.out.println("Grade: C");
+        } else if (percentage >= 33) {
+            System.out.println("Grade: D");
+        } else {
+            System.out.println("FAIL");
+        }
+
+        System.out.println("----------Student is passed: ----------");
+        boolean isPassed ;
+        if (percentage >= 33) {
+            isPassed = true;
+        } else {
+            isPassed = false;
+        }
+        System.out.println("Is the student passed: " + isPassed);
+
+        System.out.println("----------Student Fee details:-----------");
+        int baseFees = 30000;
+        System.out.println("Base Fees: " + baseFees);
+        double discount;
+        double finalFees;
+        if (percentage >= 75) {
+            System.out.println("Scholarship: 20%");
+             discount = baseFees * 20/100;
+            System.out.println("Discount Amount: " + discount);
+             finalFees = baseFees - discount;
+             System.out.println("Final Fess : " + finalFees);
+        } else {
+            System.out.println("Scholarship: 0%");
+             discount = 0;
+            System.out.println("Discount Amount: " + discount);
+             finalFees = baseFees - discount;
+             System.out.println("Final Fess : " + finalFees);
+        }
+            input.close();
+    }
+}
